f075ee1ea7319b5564de0126799566e3daeb5aa5
Jav <j.a.cabrerapuente@gmail.com>
2019-11-14 16:44:19 -0600

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { MainComponent } from './main/main.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainComponent },
+  { path: 'pagos', component: CheckoutComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+export const routingComponents = [ AppComponent,
+                                   MainComponent,
+                                   CheckoutComponent ]
