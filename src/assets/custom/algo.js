21799513d38549a3a92903f78ae04cc8157cfb08
Fernando Pensamiento <fernanpensa@gmail.com>
2019-11-15 10:17:58 -0600

    js
    jj

---
+++ src/assets/custom/algo.js
@@ -0,0 +1,13 @@
+function hola() {
+
+    var x = document.getElementsByClassName("no_imprmir");
+
+    for (var a = 0; a < x.length; a++) {
+        x[a].style.visibility = "hidden";
+    }
+
+
+    print();
+
+
+}
